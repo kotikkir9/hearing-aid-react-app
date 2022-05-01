00d2b503c855e9d34acf25908b44531fd613d50c
Kirill Zhukau <67194208+kotikkir9@users.noreply.github.com>
2022-05-01 23:48:41 +0200

    done for today

--- src/pages/Connect.js
+++ src/pages/Connect.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import classes from './Connect.module.css';
-import GuideImage from '../assets/img/connect-guide.png';
+import ConnectGuideImage from '../assets/img/connect-guide.png';
 import { useNavigate } from 'react-router-dom';
 import Button from '../components/Button';
 
@@ -8,7 +8,11 @@ export default function Connect() {
 	const navigate = useNavigate();
 
 	const returnHandler = () => {
-		navigate('/');
+		navigate('/', { replace: true });
+	}
+
+	const connectHandler = () => {
+		navigate('/dashboard', { replace: true });
 	}
 
 	return (
@@ -18,16 +22,16 @@ export default function Connect() {
 					<ion-icon name="arrow-back-outline" class={classes.icon} onClick={returnHandler}></ion-icon>
 					<h1>Oticon More 1</h1>
 				</div>
-				<img src={GuideImage} alt="Hearing aid connect guide" className={classes.image} />
+				<img src={ConnectGuideImage} alt="Hearing aid connect guide" className={classes.image} />
 			</header>
 
 			<div className={classes['body-container']}>
-				<div className={classes['text-box']}>
+				<div className={classes['text']}>
 					<h2>Pair with your phone</h2>
-					<p className={classes.text}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum error, obcaecati voluptatum voluptatibus rem iste, perspiciatis illum neque aut, id veniam magnam.</p>
-					<p className={classes.text}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum error, obcaecati voluptatum voluptatibus rem iste, perspiciatis illum neque aut, id veniam magnam.</p>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum error, obcaecati voluptatum voluptatibus rem iste, perspiciatis illum neque aut, id veniam magnam.</p>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum error, obcaecati voluptatum voluptatibus rem iste, perspiciatis illum neque aut, id veniam magnam.</p>
 				</div>
-				<Button>Connect</Button>
+				<Button onClick={connectHandler}>Connect</Button>
 			</div>
 		</div>
 	)
